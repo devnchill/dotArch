2c601e06d556f5d8dbb34a73672bb4250a3b7515
0xDev <169875752+devnchill@users.noreply.github.com>
2025-05-12 19:31:12 +0530

    chore: changed nvim colorscheme to vague.nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,46 +1,89 @@
 return {
+  -- Lazy
   {
-    "catppuccin/nvim",
-    priority = "1000",
-    opts = {
-      color_overrides = {
-        mocha = {
-          base = "#000000",
-          mantle = "#000000",
-          crust = "#000000",
+    "vague2k/vague.nvim",
+    config = function()
+      -- NOTE: you do not need to call setup if you don't want to.
+      require("vague").setup({
+        style = {
+          boolean = "bold",
+          number = "none",
+          float = "none",
+          error = "bold",
+          comments = "italic",
+          conditionals = "none",
+          functions = "none",
+          headings = "bold",
+          operators = "none",
+          strings = "italic",
+          variables = "none",
+
+          -- keywords
+          keywords = "none",
+          keyword_return = "italic",
+          keywords_loop = "none",
+          keywords_label = "none",
+          keywords_exception = "none",
+
+          -- builtin
+          builtin_constants = "bold",
+          builtin_functions = "none",
+          builtin_types = "bold",
+          builtin_variables = "none",
         },
-      },
-      styles = {
-        comments = {},
-        conditionals = {},
-        loops = {},
-        functions = { "bold" },
-        keywords = { "italic" },
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = { "italic" },
-        properties = {},
-        types = { "underline" },
-      },
-      highlight_overrides = {
-        mocha = function(colors)
-          return {
-            WinSeparator = { fg = colors.overlay2, bg = "#000000" },
-            FloatBorder = { fg = colors.overlay2, bg = "#000000" },
-            NormalFloat = { bg = "#000000" },
-            PmenuSel = { bg = "#3FCF8E", fg = colors.crust },
-          }
-        end,
-      },
-    },
+        plugins = {
+          cmp = {
+            match = "bold",
+            match_fuzzy = "bold",
+          },
+          dashboard = {
+            footer = "italic",
+          },
+          lsp = {
+            diagnostic_error = "bold",
+            diagnostic_hint = "none",
+            diagnostic_info = "italic",
+            diagnostic_warn = "bold",
+          },
+          neotest = {
+            focused = "bold",
+            adapter_name = "bold",
+          },
+          telescope = {
+            match = "bold",
+          },
+        },
+        colors = {
+          bg = "#000",
+          fg = "#cdcdcd",
+          floatBorder = "#878787",
+          line = "#252530",
+          comment = "#606079",
+          builtin = "#b4d4cf",
+          func = "#c48282",
+          string = "#e8b589",
+          number = "#e0a363",
+          property = "#c3c3d5",
+          constant = "#aeaed1",
+          parameter = "#bb9dbd",
+          visual = "#333738",
+          error = "#df6882",
+          warning = "#f3be7c",
+          hint = "#7e98e8",
+          operator = "#90a0b5",
+          keyword = "#6e94b2",
+          type = "#9bb4bc",
+          search = "#405065",
+          plus = "#8cb66d",
+          delta = "#f3be7c",
+        },
+      })
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = function()
-        require("catppuccin").load()
-      end,
+      colorscheme = "vague",
     },
   },
 }
