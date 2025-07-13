1b9454b275b782b2f720092f0fa7c8e99db57d48
0xDev <169875752+devnchill@users.noreply.github.com>
2025-07-13 14:29:03 +0530

    fix: syntax highlighting for ejs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.linespace = 2
+
+vim.filetype.add({
+  extension = {
+    ejs = "embedded_template",
+  },
+})
